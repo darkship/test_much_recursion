066b62d16a4df230463b017a6841ba0ef34ac594
tkraebihl <tkraebihl@tiz.fr>
2015-01-15 17:44:21 +0100

    first commit

---
+++ test_much_recursion.js
@@ -0,0 +1,43 @@
+if (Meteor.isServer) {
+    return;
+}
+
+
+function item_generator(n) {
+    var baseItem = {number: 1}
+    if (n<1) {
+        return
+
+    }
+    else{
+        var lastItem=baseItem
+        for(var i=1;i<n;i++)
+        {
+            lastItem.item={number: i+1}
+            lastItem=lastItem.item
+        }
+    }
+
+    return baseItem;
+}
+Session.setDefault("recursions",3)
+Template.body.events({
+    'change input':function(evt,tpl)
+    {
+        Session.set("recursions",parseInt(evt.target.value))
+    }
+})
+Template.body.helpers({
+    recursions:function()
+    {
+        return Session.get("recursions")
+    },
+    item: function () {
+        return item_generator(Session.get("recursions"))
+
+    }
+})
+
+
+
+
